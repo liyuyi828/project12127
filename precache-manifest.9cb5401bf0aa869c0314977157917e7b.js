d486edf9ee7d15f41a814aea7d6e2aae72969e6f
Yuyi <liyuyi828@gmail.com>
2020-06-27 22:56:31 -0700

    Updates

---
+++ precache-manifest.9cb5401bf0aa869c0314977157917e7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "432783d1fd1e2555c13c951ad6c62be7",
+    "url": "/project12127/index.html"
+  },
+  {
+    "revision": "b4a3ff187634de2c7f2c",
+    "url": "/project12127/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "426b098753b8cbc54d21",
+    "url": "/project12127/static/js/2.b03698d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project12127/static/js/2.b03698d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4a3ff187634de2c7f2c",
+    "url": "/project12127/static/js/main.cae54f82.chunk.js"
+  },
+  {
+    "revision": "6f785d0df4c9fb457852",
+    "url": "/project12127/static/js/runtime-main.b121fe9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project12127/static/media/logo.5d5d9eef.svg"
+  }
+]);
